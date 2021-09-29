d46f2cd1a157054bcbb6d08ed10359c4e48415ea
Chester How <chesterhow@gmail.com>
2021-09-29 13:09:30 +0800

    build(deps): set up redux and persist

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+
+import chatSlice from './chatSlice';
+
+const reducers = combineReducers({
+  chat: chatSlice,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
