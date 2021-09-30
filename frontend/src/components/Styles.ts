4bccb44425b1fe504d4e7d685d34a412541497b5
artlee06 <ykarthurlee@gmail.com>
2021-09-30 11:42:15 +0800

    style: Styles according to figma mock

--- frontend/src/components/Styles.ts
+++ frontend/src/components/Styles.ts
@@ -4,3 +4,13 @@ import styled from 'styled-components';
 export const PageLayout = styled(Layout)`
   min-height: 100vh;
 `;
+
+interface SpacerProps {
+  height?: string;
+  width?: string;
+}
+
+export const Spacer = styled.div<SpacerProps>`
+  width: ${(props) => (props.width ? props.width : 0)};
+  height: ${(props) => (props.height ? props.height : 0)};
+`;
