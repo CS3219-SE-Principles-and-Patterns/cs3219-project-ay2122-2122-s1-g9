5e72a026f3a2dee09bb0254e73025c9e46b11aaa
Azeem Vasanwala <azeem.iron@gmail.com>
2021-10-11 18:33:57 +0800

    Add support to deal with multiple users in the queue

--- backend/functions/src/match.ts
+++ backend/functions/src/match.ts
@@ -3,42 +3,63 @@ import * as admin from 'firebase-admin';
 
 export const detectMatchCreateSession = functions.database
   .ref('/queues/{difficulty}')
-  .onCreate((snap, context) => {
+  .onWrite((change, context) => {
     const queueName = context.params.difficulty;
-    let queue = snap.val();
-    console.log(queue);
+
+    // Exit when the data is deleted.
+    if (!change.after.exists()) {
+      return null;
+    }
+
+    const queue = change.after.val();
 
     // queue should not be empty
     if (queue == null) {
-      return;
+      return null;
     }
 
     // queue should only have 2 people in it
-    if (queue.length != 2) {
-      console.log(`queue ${queueName} does not have exactly 2 people`);
-      return;
+    if (queue.length < 2) {
+      functions.logger.log(
+        `queue ${queueName} does not have at least 2 people`
+      );
+      return null;
     }
 
-    let sessionPath = admin.database().ref('/sessions');
-    let userPath = admin.database().ref('/users');
-    let queuePath = admin.database().ref('/queues');
+    const sessionPath = admin.database().ref('/sessions');
+    const userPath = admin.database().ref('/users');
+    const queuePath = admin.database().ref('/queues');
 
-    // First we create a session
-    const session = {
-      users: [...queue],
-      time: Date.now(),
-    };
-    const sessionId = sessionPath.push(session).key;
-    // then we add the session to the user's message queue
-    const notification = {
-      type: 'FOUND_SESSION',
-      session_id: sessionId,
-    };
-    userPath.child(queue[0]).push(notification);
-    userPath.child(queue[1]).push(notification);
+    // We will make 1 match for every 2 people in the queue
+    const numOfMatches = Math.floor(queue.length / 2);
+    let count = 0;
 
-    // Empty the queue
-    queuePath.child(queueName).remove();
+    while (count < numOfMatches) {
+      const userOne = queue[0];
+      const userTwo = queue[1];
 
-    return;
+      // First we create a session
+      const session = {
+        users: [userOne, userTwo],
+        createdAt: Date.now(),
+      };
+      const sessionId = sessionPath.push(session).key;
+      // then we add the session to the user's message queue
+      const notification = {
+        type: 'FOUND_SESSION',
+        session_id: sessionId,
+      };
+      userPath.child(userOne).push(notification);
+      userPath.child(userTwo).push(notification);
+
+      // Pop the first 2 elements from the queue
+      queue.shift();
+      queue.shift();
+      count++;
+    }
+
+    // Update the queue to only contain the unmatched people
+    queuePath.child(queueName).set(queue);
+
+    return null;
   });
