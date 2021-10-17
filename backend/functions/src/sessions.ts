116bdbf72422eaa2e7357c509e5bdc6881e8fa8d
Ivan Tan <ivantjh97@gmail.com>
2021-10-17 14:39:12 +0800

    chore: run formatter

--- backend/functions/src/sessions.ts
+++ backend/functions/src/sessions.ts
@@ -6,40 +6,39 @@ export const initSession = functions.database
   .onCreate(
     (
       snapshot: functions.database.DataSnapshot,
-			_context: functions.EventContext
+      _context: functions.EventContext
     ) => {
-			const db = admin.database();
+      const db = admin.database();
 
-			const sessPath = snapshot;
-			const users: string[] = sessPath.child('users').val();
+      const sessPath = snapshot;
+      const users: string[] = sessPath.child('users').val();
 
-			if (users.length != 2) {
-				throw new functions.https.HttpsError(
+      if (users.length != 2) {
+        throw new functions.https.HttpsError(
           'failed-precondition',
           `There should only be 2 users in a session. Found ${users.length} at session ${sessPath.key}.`
         );
-			}
+      }
 
-			// Write Default Code
-			const writeDefaultCodeNotif = {
-				type: 'WRITE_DEFAULT_CODE',
-				sess_id: sessPath.key,
-				qns_id: 'two-sum', // this could be derived from this function or the matches one
-			};
-		
-			const userToWrite = users[0];
-			db.ref(`/users/${userToWrite}`).push(writeDefaultCodeNotif)
-		
-			// Add User To Session
+      // Write Default Code
+      const writeDefaultCodeNotif = {
+        type: 'WRITE_DEFAULT_CODE',
+        sess_id: sessPath.key,
+        qns_id: 'two-sum', // this could be derived from this function or the matches one
+      };
+
+      const userToWrite = users[0];
+      db.ref(`/users/${userToWrite}`).push(writeDefaultCodeNotif);
+
+      // Add User To Session
       const foundSessNotif = {
         type: 'FOUND_SESSION',
         sess_id: sessPath.key,
       };
 
-			for (const user of users) {
-				const userPath = db.ref(`/users/${user}`);
-				userPath.push(foundSessNotif);
-			}
-		}
+      for (const user of users) {
+        const userPath = db.ref(`/users/${user}`);
+        userPath.push(foundSessNotif);
+      }
+    }
   );
-
