2df5260ffb5be4f258b7282e4216216c62012e72
Ivan Tan <ivantjh97@gmail.com>
2021-10-30 10:25:11 +0800

    wip

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,11 +1,17 @@
 import 'firebase/database';
 
-import { FirepadEvent, fromMonaco } from '@hackerrank/firepad';
+import {
+  FirebaseAdapter,
+  Firepad,
+  FirepadEvent,
+  fromMonaco,
+  MonacoAdapter,
+} from '@hackerrank/firepad';
 import MonacoEditor, { EditorProps, Monaco } from '@monaco-editor/react';
 import { message } from 'antd';
 import firebase from 'firebase/app';
 import { editor } from 'monaco-editor';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import firebaseApp from '../firebase/firebaseApp';
@@ -37,7 +43,6 @@ const Editor: React.FC<PeerprepEditorProps> = function ({
   questionTemplates,
 }) {
   const sessionId = useAppSelector(getSessionId);
-  const monacoRef = useRef<Monaco | null>(null);
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
   const sessDbRef = firebaseApp.database().ref(`/sessions/${sessionId}`);
   const currentUser = firebaseApp.auth().currentUser;
@@ -47,22 +52,142 @@ const Editor: React.FC<PeerprepEditorProps> = function ({
     questionTemplates[0].value
   );
 
+  const defaultCodeWrittenInitialState = useMemo(() => {
+    const state: { [key: string]: boolean } = {};
+    questionTemplates.forEach((template) => {
+      state[template.value] = false;
+    });
+
+    return state;
+  }, [questionTemplates]);
+
+  const [defaultCodeWritten, setDefaultCodeWritten] = useState<{
+    [key: string]: boolean;
+  }>(defaultCodeWrittenInitialState);
+
   const options: editor.IStandaloneEditorConstructionOptions = {
     cursorStyle: 'line',
   };
+
+  // useEffect(() => {
+  //   return () => {
+  //     editorRef.current = null;
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   if (!editorLoaded || editorRef.current == null || !editorLanguage) {
+  //     return;
+  //   }
+
+  //   // const contentRef = sessDbRef.child(`content/${editorLanguage}`);
+  //   const contentRef = firebaseApp
+  //     .database()
+  //     .ref(`/sessions/${sessionId}/content/${editorLanguage}`);
+
+  //   sessDbRef
+  //     .child('defaultWriter')
+  //     .get()
+  //     .then((snapshot) => {
+  //       if (editorRef.current == null) {
+  //         return;
+  //       }
+
+  //       const defaultWriterUid = snapshot.val(); // guaranteed to be inside because written by backend
+  //       const defaultWriter = defaultWriterUid === currentUser?.uid; // currentUser guaranteed to be there
+
+  //       if (defaultWriter) {
+  //         editorRef.current?.setValue('');
+  //       }
+
+  //       const firepad = fromMonaco(contentRef, editorRef.current);
+  //       if (currentUser?.displayName) {
+  //         firepad.setUserName(currentUser.displayName);
+  //       }
+  //     });
+
+  //   const firepadOnReady = (_ready: boolean) => {
+  //     console.log(_ready);
+  //     sessDbRef
+  //       .child('defaultWriter')
+  //       .get()
+  //       .then((snapshot) => {
+  //         const defaultWriterUid = snapshot.val(); // guaranteed to be inside because written by backend
+  //         const defaultWriter = defaultWriterUid === currentUser?.uid; // currentUser guaranteed to be there
+
+  //         if (defaultWriter) {
+  //           const codeToWrite =
+  //             questionTemplates.find(
+  //               (language) => language.value === editorLanguage
+  //             )?.defaultCode ?? '';
+
+  //           // Rewrite history
+
+  //           // console.log('i am default writer');
+
+  //           // This will be the last setText executed throughout the initialization of firepad and related libraries.
+  //           if (!defaultCodeWritten[editorLanguage]) {
+  //             firepad.setText(codeToWrite);
+
+  //             const newState = { ...defaultCodeWritten };
+  //             newState[editorLanguage] = true;
+  //             setDefaultCodeWritten(newState);
+  //           }
+
+  //           // if (firepad.isHistoryEmpty()) {
+  //           //   console.log('writing default code');
+  //           //   firepad.setText(codeToWrite);
+  //           // }
+  //         }
+  //       });
+  //   };
+
+  //   firepad.on(FirepadEvent.Ready, firepadOnReady);
+  //   return () => {
+  //     firepad.off(FirepadEvent.Ready, firepadOnReady);
+  //     firepad.dispose();
+  //   };
+
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [editorLoaded, editorLanguage, questionTemplates]);
 
   useEffect(() => {
     if (!editorLoaded || editorRef.current == null || !editorLanguage) {
       return;
     }
 
-    const contentRef = sessDbRef.child(`content/${editorLanguage}`);
-    const firepad = fromMonaco(contentRef, editorRef.current);
-    if (currentUser?.displayName) {
-      firepad.setUserName(currentUser.displayName);
+    if (!currentUser) {
+      console.log(
+        'Clause should not be entered. Need currentUser to init firepad'
+      );
+      return;
     }
 
-    const firepadOnReady = () => {
+    // const contentRef = sessDbRef.child(`content/${editorLanguage}`);
+    const contentRef = firebaseApp
+      .database()
+      .ref(`/sessions/${sessionId}/content/${editorLanguage}`);
+    // const firepad = fromMonaco(contentRef, editorRef.current);
+
+    const databaseAdapter = new FirebaseAdapter(
+      contentRef,
+      currentUser.uid,
+      '#000000',
+      currentUser.displayName ?? 'Unknown username'
+    );
+    const editorAdapter = new MonacoAdapter(editorRef.current, false);
+    const firepad = new Firepad(databaseAdapter, editorAdapter, {
+      userId: currentUser.uid,
+      userName: currentUser.displayName ?? 'Unknown username',
+      userColor: '#000000',
+      defaultText: '', // important line
+    });
+
+    // if (currentUser?.displayName) {
+    //   firepad.setUserName(currentUser.displayName);
+
+    const firepadOnReady = (_ready: boolean) => {
+      console.log(_ready);
       sessDbRef
         .child('defaultWriter')
         .get()
@@ -76,7 +201,21 @@ const Editor: React.FC<PeerprepEditorProps> = function ({
                 (language) => language.value === editorLanguage
               )?.defaultCode ?? '';
 
+            // Rewrite history
+
+            // console.log('i am default writer');
+
+            // This will be the last setText executed throughout the initialization of firepad and related libraries.
+            // if (!defaultCodeWritten[editorLanguage]) {
+            //   firepad.setText(codeToWrite);
+
+            //   const newState = { ...defaultCodeWritten };
+            //   newState[editorLanguage] = true;
+            //   setDefaultCodeWritten(newState);
+            // }
+
             if (firepad.isHistoryEmpty()) {
+              console.log('writing default code');
               firepad.setText(codeToWrite);
             }
           }
@@ -109,10 +248,10 @@ const Editor: React.FC<PeerprepEditorProps> = function ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // only attach once
 
-  const handleEditorMount: EditorProps['onMount'] = (editor, monaco) => {
+  const handleEditorMount: EditorProps['onMount'] = (editor, _monaco) => {
     editorRef.current = editor;
-    monacoRef.current = monaco;
     setEditorLoaded(true);
+    console.log('fuck');
   };
 
   const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
