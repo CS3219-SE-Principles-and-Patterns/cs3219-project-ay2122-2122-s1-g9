be9571357326094f069a18f7d134dd9214133fc6
Ivan Tan <ivantjh97@gmail.com>
2021-11-07 00:08:24 +0800

    chore: update env variable for db url

--- frontend/src/firebase/firebaseOptions.ts
+++ frontend/src/firebase/firebaseOptions.ts
@@ -2,7 +2,7 @@ import { FirebaseOptions } from '@firebase/app-types';
 
 const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY || '';
 const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '';
-const FIREBASE_DATABASE_URL = process.env.REACT_APP_DATABASE_URL || '';
+const FIREBASE_DATABASE_URL = process.env.REACT_APP_FIREBASE_DATABASE_URL || '';
 const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID || '';
 const FIREBASE_STORAGE_BUCKET =
   process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '';
