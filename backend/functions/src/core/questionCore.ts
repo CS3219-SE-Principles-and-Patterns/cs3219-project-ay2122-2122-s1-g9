ca61a17593da788a100168940afbf0c64ed12e36
Ivan Tan <ivantjh97@gmail.com>
2021-11-09 16:09:37 +0800

    fix: fix bugs with triggering outdated tasks

--- backend/functions/src/core/questionCore.ts
+++ backend/functions/src/core/questionCore.ts
@@ -1,31 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import { randomBytes } from 'crypto';
 import { ALL_LVLS, LVL_EASY, LVL_MEDIUM, LVL_HARD } from '../consts/values';
-
-// Adapted from https://github.com/firebase/firebase-js-sdk/blob/master/packages/firestore/src/util/misc.ts
-function autoId(): string {
-  // Alphanumeric characters
-  const chars =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  // The largest byte value that is a multiple of `char.length`.
-  const maxMultiple = Math.floor(256 / chars.length) * chars.length;
-
-  let autoId = '';
-  const targetLength = 20;
-  while (autoId.length < targetLength) {
-    const bytes = randomBytes(40);
-    for (let i = 0; i < bytes.length; ++i) {
-      // Only accept values that are [0, maxMultiple), this ensures they can
-      // be evenly mapped to indices of `chars` via a modulo operation.
-      if (autoId.length < targetLength && bytes[i] < maxMultiple) {
-        autoId += chars.charAt(bytes[i] % chars.length);
-      }
-    }
-  }
-
-  return autoId;
-}
+import autoId from '../util/autoId';
 
 export async function getRandomQuestion(level: string): Promise<string> {
   // See https://stackoverflow.com/questions/46798981/firestore-how-to-get-random-documents-in-a-collection for more info
--- backend/functions/src/queues.ts
+++ backend/functions/src/queues.ts
@@ -32,12 +32,15 @@ export const removeUserFromQueue = functionBuilder.https.onCall(
 // This function will be called by Cloud Tasks so we use onRequest.
 export const removeUnmatchedUserAfterTimeout = functionBuilder.https.onRequest(
   async (req: functions.Request, res: functions.Response) => {
+    // The status should be all 200 or else the task will automatically be retried.
+    // We are using the same error format as functions.https.HttpsError to be consistent
+
     functions.logger.info(`Received parameters ${JSON.stringify(req.body)}`);
 
     const data = req.body.data;
 
     if (!data || !data.userId || !data.queueName) {
-      res.status(400).json({
+      res.status(200).json({
         code: 'invalid-argument',
         message:
           'The function must be called with two arguments queueName and userId.',
@@ -45,13 +48,33 @@ export const removeUnmatchedUserAfterTimeout = functionBuilder.https.onRequest(
       return;
     }
 
-    // If user is in a session, do nothing
     const userId = data.userId;
+    const matchTimeoutTask = await queueCore.getMatchTimeoutTask(userId);
+
+    // Make sure that the task is not outdated
+    if (
+      !matchTimeoutTask ||
+      (matchTimeoutTask && matchTimeoutTask.taskId !== data.taskId)
+    ) {
+      const msg = `Received outdated task for user ${userId}. Aborting`;
+      functions.logger.info(msg);
+      res.status(200).json({
+        code: 'failed-precondition',
+        message: msg,
+      });
+      return;
+    }
+
+    // Remove task that is going to be processed
+    await queueCore.removeMatchTimeoutTask(userId);
+
+    // If user is in a session, do nothing
     const isUserInSession = await isInCurrentSession(userId);
     if (isUserInSession) {
       const msg = `User ${userId} is already in a session and will not be removed from the queue`;
       functions.logger.info(msg);
       res.status(200).json({
+        code: 'failed-precondition',
         message: msg,
       });
       return;
@@ -60,7 +83,7 @@ export const removeUnmatchedUserAfterTimeout = functionBuilder.https.onRequest(
     // If the user is not in a session, remove them from the queue
     const queueName = queueCore.validateAndGetLevel(data);
     if ((await queueCore.getQueueUserIsIn(userId)) !== queueName) {
-      res.status(400).json({
+      res.status(200).json({
         code: 'failed-precondition',
         message: `UserId ${userId} is not in ${queueName} queue`,
       });
